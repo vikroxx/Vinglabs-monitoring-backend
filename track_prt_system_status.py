beb30b3ae9f035802b2b2bdedc3b74f6d205b922
vikroxx <vikrant.saxena94@gmail.com>
2022-10-13 02:38:18 +0530

    12_10, added track prt system tracking

---
+++ track_prt_system_status.py
@@ -0,0 +1,71 @@
+import requests
+import time
+import psycopg2
+from datetime import datetime, timedelta, timezone
+
+
+db_table_name = 'aggregate_new'
+timestamp_format = "%d%m%Y-%H%M"
+timestamp_format_trends = "%d%m%Y-%H%M%S"
+
+
+def alert_message():
+    api_key = "8fcef776e875fac1a2f14ae10692d9c7-b0ed5083-da3fe118"
+    domain_name = "sandbox3de5f88518024eadbf89034a019a32df.mailgun.org"
+    # print('drawn participant :\n', text_string)
+    return requests.post(
+        "https://api.mailgun.net/v3/{}/messages".format(domain_name),
+        auth=("api", api_key),
+        data={"from": "Mailgun <mailgun@vinglabs.com>",
+              "to": ["vinglabs", "contact@vinglabs.com"],
+              "subject": "PRT System - Stopped working!",
+              "text": "Kuch fat gaya bhai!!\nCheck kar lo!"})
+
+
+def main():
+    last_fail_time = False
+    while True:
+        try:
+            cursor = psycopg2.connect(
+                database="prt-db",
+                user="postgres",
+                password="biology12",
+                host="prt-prod.cmuk1jsd4tyu.ap-south-1.rds.amazonaws.com",
+                port='5432').cursor()
+
+            query = "SELECT " + 'datetime' + " FROM " + db_table_name + " order by datetime desc limit 10"
+            # print(query)
+            try:
+                cursor.execute(query)
+            except Exception as err:
+                print(err)
+            results = cursor.fetchall()
+            if bool(results):
+                timestamps = list(map(list, zip(*results)))
+                # print(timestamps[0][0])
+                current_datetime = datetime.now(timezone.utc) + timedelta(hours=2)
+                delta = current_datetime.replace(tzinfo=None) - timestamps[0][0].replace(tzinfo=None)
+                # print(delta)
+                if delta > timedelta(minutes=2):
+                    fail_time = timestamps[0][0]
+                    if not last_fail_time:
+                        last_fail_time = fail_time
+                        alert_message()
+                        print('kaand ho gaya!')
+                    else:
+                        if last_fail_time == fail_time:
+                            print('Already intimated!')
+                        else:
+                            alert_message()
+                            print('kaand ho gaya!')
+                else:
+                    print('{} : Sab changa si..'.format(timestamps[0][0]))
+
+            time.sleep(10)
+        except Exception as e:
+            print('Failed due to error {}'.format(e))
+            time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
