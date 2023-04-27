d54c60240758ca0a9f194b43e7f305299778727e
vikroxx <vikrant.saxena94@gmail.com>
2023-04-28 05:15:17 +0530

    updated database

--- fastapi_app.py
+++ fastapi_app.py
@@ -15,7 +15,7 @@ con = psycopg2.connect(
     database="prt-db",
     user="postgres",
     password="biology12",
-    host="prt-prod.cmuk1jsd4tyu.ap-south-1.rds.amazonaws.com",
+    host="http://prt-prod.cgmaehois3kz.ap-south-1.rds.amazonaws.com",
     port='5432'
 )
 # cursor = con.cursor()
