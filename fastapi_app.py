fa58c106f84ff1a0172b1a7b00c8c6b9ee6f42dd
Vikrant Saxena <75198801+vikroxx@users.noreply.github.com>
2024-04-28 16:38:40 +0530

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -14,7 +14,7 @@ app = FastAPI()
 con = psycopg2.connect(
     database="prt-db",
     user="postgres",
-    password="biology12",
+    password="XXXXXXXX",
     host="prt-prod.cgmaehois3kz.ap-south-1.rds.amazonaws.com",
     port='5432'
 )
